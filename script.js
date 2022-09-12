8865aed3142eff113f973c017ac775eb4c2273ad
Yassine <69854948+Buckyball-s@users.noreply.github.com>
2022-09-12 08:54:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,630 @@
+let datasheet=[
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€127.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    },
+    {
+        "date": "Sep 5 - 12 Sep",
+        "total": "€12888.99",
+        "days": {
+            "Monday":[5,100],
+            "Tuesday":[6,82],
+            "Wednesday":[7,0],
+            "Thurdsay":[8,50],
+            "Friday":[9,60],
+            "Saturday":[10,0],
+            "Sunday":[11,0]
+        }
+    }
+];
+
+let weeklyEarnings = document.getElementById("weeklyEarnings");
+
+for (let i = 0; i < 10; i++)
+{
+//     console.log(i.days.M[1])
+    newWeek(datasheet[i]);
+}
+
+function newWeek(data){
+    let week = document.createElement("li");
+    week.innerHTML=`
+    <div>
+        <p class="date">
+            ${data.date}
+        </p>
+        <p class="earnings">
+            ${data.total}
+        </p>
+    </div>
+    <div class="hours">
+        <div class="day">
+            <div style="height:${data.days.Monday[1]}%">
+            </div>
+            <p>
+                ${data.days.Monday[0]}
+            </p>
+        </div>
+                <div class="day">
+            <div style="height:${data.days.Tuesday[1]}%">
+            </div>
+            <p>
+                ${data.days.Tuesday[0]}
+            </p>
+        </div>
+        <div class="day">
+            <div style="height:${data.days.Wednesday[1]}%">
+            </div>
+            <p>
+                ${data.days.Wednesday[0]}
+            </p>
+        </div>
+        <div class="day">
+            <div style="height:${data.days.Thurdsay[1]}%">
+            </div>
+            <p>
+                ${data.days.Thurdsay[0]}
+            </p>
+        </div>
+        <div class="day">
+            <div style="height:${data.days.Friday[1]}%">
+            </div>
+            <p>
+                ${data.days.Friday[0]}
+            </p>
+        </div>
+        <div class="day">
+            <div style="height:${data.days.Saturday[1]}%">
+            </div>
+            <p>
+                ${data.days.Saturday[0]}
+            </p>
+        </div>
+        <div class="day">
+            <div style="height:${data.days.Sunday[1]}%">
+            </div>
+            <p>
+                ${data.days.Sunday[0]}
+            </p>
+        </div>
+    </div>`
+    weeklyEarnings.appendChild(week);
+}
+let counter= 10;
+document.addEventListener("scroll", (e)=>{
+    document.removeEventListener("scroll", (e)=>{});
+//     console.log(window.scrollY + window.innerHeight);
+    if (window.scrollY + window.innerHeight == document.body.scrollHeight)
+    {
+//         console.log("scrolling end");
+        if (counter + 10 < datasheet.length)
+        {
+            let loadingIcon= document.createElement("div")
+            loadingIcon.innerHTML="<p> loading </p>";
+            weeklyEarnings.appendChild(loadingIcon);
+            setInterval(()=>{
+                weeklyEarnings.removeChild(loadingIcon);
+                for(let i = 0; i< 10; i++)
+                {
+                    {
+                        newWeek(datasheet[counter+i]);
+                    }
+                }
+                counter+=10;
+            },2500);
+        }
+        else{
+            console.log("moins de 10 weeks restantes", datasheet.length);
+            for(let i = 0; i< counter-datasheet.length; i++){
+                newWeek(datasheet[counter+i]);
+            }
+            document.removeEventListener("scroll", (e)=>{
+                console.log("event listener removed!");
+            });
+        }
+    };
+
+});
